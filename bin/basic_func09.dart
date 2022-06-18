c9ff13704059f0af0847f47773fe7c53b985c8b3
Shohrux <shohruxergashev1204@gmail.com>
2022-06-18 17:03:48 +0500

    basic_func09.dart

--- bin/basic_func09.dart
+++ bin/basic_func09.dart
@@ -1,6 +1,11 @@
 //  Create a function called func.
-
+int func (a){
+return a-1;
+}
 //  Create function arguments a.
 
 //  decrease the value of a to one and return
-void main() {}
+void main(){
+ print (func(5));
+} 
+
