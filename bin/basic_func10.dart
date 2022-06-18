7f9bc215df3e094dd70ea57c0b2912210788dcc2
Shohrux <shohruxergashev1204@gmail.com>
2022-06-18 17:09:09 +0500

    basic_fun10.dart

--- bin/basic_func10.dart
+++ bin/basic_func10.dart
@@ -1,6 +1,10 @@
 //  Create a function called func.
-
+int func (a){
+    return -a;
+}
 //  Create function arguments a.
 
 //  Return the negative value of a.
-void main() {}
+void main() {
+    print (func(5));
+}
