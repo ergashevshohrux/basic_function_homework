7f71c26accfe1a3e87d0f12c20474f8e4005d71f
Shohrux <shohruxergashev1204@gmail.com>
2022-06-18 16:59:46 +0500

    basic_func08.dart

--- bin/basic_func08.dart
+++ bin/basic_func08.dart
@@ -1,6 +1,10 @@
 //  Create a function called func.
-
+int  func(a){
+return a+1;
+}
 //  Create function arguments a.
 
 //  Increase the value of a to one and return.
-void main() {}
+void main() {
+    print (func(5));
+}
